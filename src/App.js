45e65d63545b18e1f3516188b728796348eb793c
DEV3L <jus.beall@gmail.com>
2018-07-14 14:55:03 -0400

    Add stateless component to represent players

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
 
+
+const PlayerCard = ({color, symbol}) => {
+    const styles = {
+        backgroundColor: color
+    };
+    return (
+        <div style={styles} className="player-card">
+            {symbol}
+        </div>
+    )
+};
+
 class App extends Component {
     render() {
         return (
             <div className="App">
-
+                <PlayerCard color="red" symbol="paper"/>
+                <PlayerCard color="blue" symbol="rock"/>
             </div>
         );
     }
